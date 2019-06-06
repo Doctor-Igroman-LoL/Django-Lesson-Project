b08df51317e5d5ec5c478ae9eeff00f8580e0451
Doctor_Bug <dr.igroman.lol@gmail.com>
2019-06-06 22:11:21 +0900

    Перезапуск проекта Джанго и иницилизация нового проекта

---
+++ Django_Dr_Bug/Webshop/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Doctor Bug Lindesh\Documents\Projects\Django\Django_Dr_Bug\Webshop\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
