2034256ffcf085e78ffc70c8d87b32fd6c9976fa
Doctor_Bug <dr.igroman.lol@gmail.com>
2019-06-10 22:32:24 +0900

    Подключение json-файла

--- Django_Dr_Bug/Webshop/geekshop/mainapp/views.py
+++ Django_Dr_Bug/Webshop/geekshop/mainapp/views.py
@@ -1,15 +1,27 @@
+# -*- coding: utf_8 -*-
 from django.shortcuts import render
+import os
+import json
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+path = BASE_DIR + '\\mainapp\\db.json'
+
+with open(path, 'r') as f:
+    DATA_JSON = json.load(f)
 
 # Create your views here.
 def main(request):
     return render(request, 'mainapp/main.html', {'username': 'doctor bug', 'array': ['e', 'r', 'r', 'o', 'r']})
 
 def products(request):
-    return render(request, 'mainapp/products.html', {'links_menu': [
+    return render(request, 'mainapp/products.html', DATA_JSON)
+
+'''
+{'links_menu': [
         {'href': 'main', 'name': 'Главная'},
         {'href': 'products', 'name': 'Продукты'},
         {'href': 'contacts', 'name': 'Контакты'},
-    ]})
+    ]}
+'''
 
 def contacts(request):
     return render(request, 'mainapp/contacts.html')
