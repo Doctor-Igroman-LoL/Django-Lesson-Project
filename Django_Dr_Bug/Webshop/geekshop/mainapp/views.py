ac640e3adf61c75b25e795dd070bcbe2d9c305cb
Doctor_Bug <dr.igroman.lol@gmail.com>
2019-06-10 21:16:51 +0900

    Добавления links_menu

--- Django_Dr_Bug/Webshop/geekshop/mainapp/views.py
+++ Django_Dr_Bug/Webshop/geekshop/mainapp/views.py
@@ -5,7 +5,11 @@ def main(request):
     return render(request, 'mainapp/main.html', {'username': 'doctor bug', 'array': ['e', 'r', 'r', 'o', 'r']})
 
 def products(request):
-    return render(request, 'mainapp/products.html')
+    return render(request, 'mainapp/products.html', {'links_menu': [
+        {'href': 'main', 'name': 'Главная'},
+        {'href': 'products', 'name': 'Продукты'},
+        {'href': 'contacts', 'name': 'Контакты'},
+    ]})
 
 def contacts(request):
     return render(request, 'mainapp/contacts.html')
