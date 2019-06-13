ac68f34ca16eb1776c70d0c0ce90cc6861a28624
Doctor_Bug <dr.igroman.lol@gmail.com>
2019-06-13 21:01:51 +0900

    Добавление категорий на страничку Продуктов

--- Django_Dr_Bug/Webshop/geekshop/mainapp/views.py
+++ Django_Dr_Bug/Webshop/geekshop/mainapp/views.py
@@ -1,27 +1,23 @@
 # -*- coding: utf_8 -*-
 from django.shortcuts import render
+from .models import Product, ProductCategory
 import os
 import json
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 path = BASE_DIR + '\\mainapp\\db.json'
+data_json = {}
 
 with open(path, 'r', encoding = "utf-8") as f:
-    DATA_JSON = json.load(f)
+    data_json = json.load(f)
+
+data_json.update({'products': Product.objects.all(), 'products_category': ProductCategory.objects.all()})
 
 # Create your views here.
 def main(request):
     return render(request, 'mainapp/main.html', {'username': 'doctor bug', 'array': ['e', 'r', 'r', 'o', 'r']})
 
 def products(request):
-    return render(request, 'mainapp/products.html', DATA_JSON)
-
-'''
-{'links_menu': [
-        {'href': 'main', 'name': 'Главная'},
-        {'href': 'products', 'name': 'Продукты'},
-        {'href': 'contacts', 'name': 'Контакты'},
-    ]}
-'''
+    return render(request, 'mainapp/products.html', data_json )
 
 def contacts(request):
     return render(request, 'mainapp/contacts.html')
