90d1a48fbab33fa6292696ad706771222e242f6c
Doctor_Bug <dr.igroman.lol@gmail.com>
2019-06-16 16:49:06 +0900

    Заполнение файла authapp/models

--- Django_Dr_Bug/Webshop/geekshop/authapp/models.py
+++ Django_Dr_Bug/Webshop/geekshop/authapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class ShopUser(AbstractUser):
+    class Meta:
+        verbose_name = 'пользователь'
+        verbose_name_plural = 'пользователи'
+
+    avatar = models.ImageField(upload_to='users_avatars', blank=true)
+    age = models.PositiveIntegerField(verbose_name='возраст')
+
+    def __str__(self):
+        return self.username
