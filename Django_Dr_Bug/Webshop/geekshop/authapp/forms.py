bde42b2dfa80d1c7b17181809441ec01835f6b46
Doctor_Bug <dr.igroman.lol@gmail.com>
2019-06-17 22:09:41 +0900

    Доделывание регистрации

--- Django_Dr_Bug/Webshop/geekshop/authapp/forms.py
+++ Django_Dr_Bug/Webshop/geekshop/authapp/forms.py
@@ -1,15 +1,15 @@
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm
 
 from .models import ShopUser
 
 
-class ShopUserLoginForm(AuthenticationForm):
+class ShopUserRegisterForm(UserCreationForm):
     class Meta:
         model = ShopUser
         fields = ('username', 'first_name', 'password1', 'password2', 'email', 'age', 'avatar')
 
     def __init__(self, *args, **kwargs):
-        super(ShopUserLoginForm, self).__init__(*args, **kwargs)
+        super().__init__(*args, **kwargs)
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'form-control'
             field.help_text = ''
--- Django_Dr_Bug/Webshop/geekshop/authapp/views.py
+++ Django_Dr_Bug/Webshop/geekshop/authapp/views.py
@@ -2,17 +2,17 @@ from django.shortcuts import render, HttpResponseRedirect
 from django.views.generic.edit import UpdateView
 from django.contrib import auth
 from django.urls import reverse
-from .forms import ShopUserLoginForm
+from .forms import ShopUserRegisterForm
 
 def register(request):
     if request.method == 'POST':
-        register_form = ShopUserLoginForm(request.POST, request.FILES)
+        register_form = ShopUserRegisterForm(request.POST, request.FILES)
         if register_form.is_valid():
             user = register_form.save()
             auth.login(request, user)
             return HttpResponseRedirect(reverse('main'))
     else:
-        register_form = ShopUserLoginForm()
+        register_form = ShopUserRegisterForm()
     context = {'form': register_form}
     return render(request, 'authapp/register.html', context)
 
@@ -23,7 +23,7 @@ def login(request):
         user = auth.authenticate(username=username, password=password)
         if user and user.is_active:
             auth.login(request, user)
-        return HttpResponseRedirect(reverse('main'))
+            return HttpResponseRedirect(reverse('main'))
     return render(request, 'authapp/login.html')
 
 def logout(request):
@@ -31,4 +31,6 @@ def logout(request):
     return HttpResponseRedirect(reverse('main'))
 
 class EditView(UpdateView):
-    pass
+    #model = ShopUser
+    template_name = 'auth/register.html'
+    #fields = 'username', 'email', 'avatar'
