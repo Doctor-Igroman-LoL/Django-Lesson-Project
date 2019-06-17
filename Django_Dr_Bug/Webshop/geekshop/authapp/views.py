178e2029fe9230468ee32eca2206cb7342218cf5
Doctor_Bug <dr.igroman.lol@gmail.com>
2019-06-17 20:43:03 +0900

    Дописан логин

--- Django_Dr_Bug/Webshop/geekshop/authapp/views.py
+++ Django_Dr_Bug/Webshop/geekshop/authapp/views.py
@@ -1,10 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
 from django.views.generic.edit import UpdateView
+from django.contrib import auth
+from django.urls import reverse
 
 def register(request):
     pass
 
 def login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = auth.authenticate(username=username, password=password)
+        if user and user.is_active:
+            auth.login(request, user)
+        return HttpResponseRedirect(reverse('main'))
+
     return render(request, 'authapp/login.html')
 
 def logout(request):
