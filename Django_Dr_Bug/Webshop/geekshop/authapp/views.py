dd09e18f0530da9ceb018438c435d24a928cfcb7
Doctor_Bug <dr.igroman.lol@gmail.com>
2019-06-17 23:43:30 +0900

    Завершение урока. Исправление ошибки редактирование

--- Django_Dr_Bug/Webshop/geekshop/authapp/views.py
+++ Django_Dr_Bug/Webshop/geekshop/authapp/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, HttpResponseRedirect
 from django.views.generic.edit import UpdateView
 from django.contrib import auth
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 from .forms import ShopUserRegisterForm
+from .models import ShopUser
 
 def register(request):
     if request.method == 'POST':
@@ -31,6 +32,7 @@ def logout(request):
     return HttpResponseRedirect(reverse('main'))
 
 class EditView(UpdateView):
-    #model = ShopUser
-    template_name = 'auth/register.html'
-    #fields = 'username', 'email', 'avatar'
+    model = ShopUser
+    template_name = 'authapp/register.html'
+    fields = 'username', 'email', 'avatar'
+    success_url = reverse_lazy('main')
